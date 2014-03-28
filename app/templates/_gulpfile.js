795868660e559f999c8ab0cbfdc245085fcf09c7
Poojan Shrestha <poojans@gmail.com>
2014-03-28 12:42:30 +0545

    Added gulp changed.

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -4,35 +4,58 @@
   var gulp = require('gulp');
   var jade = require('gulp-jade');
   var stylus = require('gulp-stylus');
+  var changed = require('gulp-changed');
   var livereload = require('gulp-livereload');
   var rjs = require('gulp-requirejs');
 
   gulp.task('jade', function () {
     var src = 'src/jade/*.jade';
-    var dest = 'src/';
+    var dest = 'html/';
 
     gulp.src(src)
         .pipe(jade())
+        .pipe(changed(dest))
         .pipe(gulp.dest(dest))
         .pipe(livereload());
   });
 
   gulp.task('stylus', function () {
-    gulp.src('src/jade/stylesheets/**/*.styl')
+    var src = 'src/jade/stylesheets/**/*.styl';
+    var dest = 'html/css/';
+
+    gulp.src(src)
         .pipe(stylus({ use: ['nib'] }))
-        .pipe(gulp.dest('./src/css'))
+        .pipe(changed(dest))
+        .pipe(gulp.dest(dest))
         .pipe(livereload());
   });
 
   gulp.task('javascript', function () {
-    gulp.src('src/js/**/*.js')
+    var src = 'src/js/**/*.js';
+    var dest = './html/js/';
+
+    gulp.src(src)
+        .pipe(changed(dest))
+        .pipe(gulp.dest(dest))
         .pipe(livereload());
   });
 
-  gulp.task('default', ['javascript', 'stylus', 'jade'], function () {
+  gulp.task('fixtures', function () {
+    var src = 'src/fixtures/**/*.json';
+    var dest = './html/fixtures/';
+
+    gulp.src(src)
+        .pipe(changed(dest))
+        .pipe(gulp.dest(dest))
+        .pipe(livereload());
+  });
+
+
+  gulp.task('default', ['javascript', 'stylus', 'jade', 'fixtures'], function () {
     gulp.watch('src/jade/*.jade', ['jade']);
     gulp.watch('src/jade/stylesheets/*.styl', ['stylus']);
     gulp.watch('src/js/**/*.js', ['javascript']);
+    gulp.watch('src/fixtures/**/*.json', ['fixtures']);
   });
 
   gulp.task('build', function () {
@@ -50,18 +73,10 @@
     gulp.src('src/js/require.js')
         .pipe(gulp.dest('build/js/'));
 
-    //gulp.src([
-      //'src/js/bootstrap.js',
-      //'src/js/app.js'
-    //])
-    //.pipe(gulp.dest('build/js/'));
-
-    //gulp.src('src/js/**/*.js')
     rjs({
       baseUrl: 'src/js/',
       name: 'main',
       out: 'main.js',
-      //mainConfigFile: 'src/js/main.js',
       paths: {
         'domReady': './src/components/requirejs-domready/domReady',
         'angular': './src/components/angular/angular'
